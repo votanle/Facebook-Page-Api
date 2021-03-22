403aeaedc36109830eba2e6f9954149aaf18800c
votanle <59087340+votanle@users.noreply.github.com>
2021-03-22 23:57:21 +0700

    first commit

---
+++ API_FB/API_FB/WebForm1.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="API_FB.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>FaceBook API</title>
+    <style type="text/css">
+        .Load_Data {
+            height: 26px;
+            background: #F0F2F5;
+            margin: 25px;
+            padding: 20px;
+            border-radius: 3px;
+        }
+        h2 {
+            font-family: 'Helvetica Neue', Arial, Helvetica, sans-serif;
+            font-size: 26px;
+            color: #ff0000;
+        }
+
+        p {
+            font-family: 'Helvetica Neue', Arial, Helvetica, sans-serif;
+            font-size: 12px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table style="width:100%;">
+                <tr>
+                    <td class="Load_Data">
+                        <asp:Label ID="lbContent" runat="server" Text="Label"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ API_FB/API_FB/WebForm1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace API_FB
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var req = WebRequest.Create("https://graph.facebook.com/utc2hcmc/posts?access_token=EAAAAZAw4FxQIBAGmKentnEWvEqAtY6ZBzi1oF0BD5zzQeAToFGZBzh26PIGnPUyrZBOb7QrZCXKuCkdwQFMpmUOzjdZBuUsApV6mqMWXdAuBzcg6g83c9GzwVXjoV1l7qdhtEx6DEbfMk8kMZA61ZAxZCm2sQ05fKOsHKOK3nsX0dfwZDZD" + "");
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+
+            Stream stream = res.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+
+            string responseString = reader.ReadToEnd();
+            dynamic data = JsonConvert.DeserializeObject(responseString);
+
+            var result = new List<content>();
+
+            foreach (var i in data.data)
+            {
+                result.Add(new content
+                {
+                    Time = i.created_time,
+                    Message = i.message,
+                    Link = i.actions[0].link
+                });
+            }
+
+            string contentString = "<h2>3 Bài đăng gần nhất</h2></br>";
+            for (int i = 0; i < 3; i++)
+            {
+                contentString += "<b></br>Bài thứ </b>" + (i + 1) + " : </br></br>";
+                contentString += "<b>Ngày đăng : </b>" + result[i].Time + "</br>";
+                contentString += "<b>Nội dung : </b>" + result[i].Message + "</br>";
+                contentString += "<b>Link bài : </b>" + result[i].Link + "</br></br>";
+                contentString += "---------------------------------------------------------";
+            }
+
+            lbContent.Text = contentString;
+        }
+    }
+}
